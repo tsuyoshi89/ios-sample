434c56acac6f613c85ed0bd9a6b385d2dbe8f1a2
tsuyoshi89 <tsuyoshi@miyano-harikyu.jp>
2013-11-27 00:40:37 -0800

    add project

---
+++ KiiSample/KiiSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KiiSample
+//
+//  Created by tsuyoshi on 2013/11/27.
+//  Copyright (c) 2013年 Tsuyoshi Miyano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MHAppDelegate class]));
+    }
+}
